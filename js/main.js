23f2be75c27ce154bf89626d6d54c987c780ac9f
pr-jack <prevgenij@gmail.com>
2019-06-11 07:14:49 +0200

    11.06

--- js/main.js
+++ js/main.js
@@ -75,14 +75,15 @@ var pictureTemplate = document.querySelector('#picture')
     .content
     .querySelector('.picture');
 
+var picturesTitleElement = document.querySelector('.pictures');
 var fragment = document.createDocumentFragment();
 
 var renderPhoto = function (photo) {
   var pictureElement = pictureTemplate.cloneNode(true);
 
-  pictureElement.querySelector('picture__img').src = photo.url;
-  pictureElement.querySelector('picture__comments').textContent = photo.comments;
-  pictureElement.querySelector('picture__likes').textContent = photo.likes;
+  pictureElement.querySelector('.picture__img').src = photo.url;
+  pictureElement.querySelector('.picture__comments').textContent = photo.comments;
+  pictureElement.querySelector('.picture__likes').textContent = photo.likes;
 
   return pictureElement;
 };
@@ -90,4 +91,5 @@ var renderPhoto = function (photo) {
 for (var i = 0; i < photos.length; i++) {
   fragment.appendChild(renderPhoto(photos[i]));
 }
-pictureTemplate.appendChild(fragment);
+
+picturesTitleElement.appendChild(fragment);
