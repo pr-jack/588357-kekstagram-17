447917dadc0816a336c3065c5c62436840b3d05a
pr-jack <prevgenij@gmail.com>
2019-07-05 17:10:01 +0200

    вроде разобрался

--- js/picture.js
+++ js/picture.js
@@ -1,11 +1,14 @@
 'use strict';
 // Создаем объект с описанием фотографий
 (function () {
+  var NEW_PICTURES = 10;
+  var DEBOUNCE_INTERVAL = 500;
   var pictureTemplate = document.querySelector('#picture')
       .content
       .querySelector('.picture');
 
   var picturesTitleElement = document.querySelector('.pictures');
+  var picturesBlock = [];
 
   var renderPhoto = function (photo) {
     var pictureElement = pictureTemplate.cloneNode(true);
@@ -26,24 +29,18 @@
     return photos;
   };
 
-  window.load(insertPhoto);
-
-  var imgFilters = document.querySelector('.img-filters');
-  imgFilters.classList.remove('img-filters--inactive');
-  var filterName = document.querySelector('.img-filters__button');
-  var imgFiltersButton = document.querySelectorAll('.img-filters__button');
-  var FilterName = {
-    POPULAR: 'filter-popular',
-    NEW: 'filter-new',
-    DISCUSSED: 'filter-discussed'
+  // функция для удаления всех элементов из родителя
+  var clearPictures = function () {
+    picturesTitleElement.querySelectorAll('.picture').forEach(function (element) {
+      element.remove();
+    });
   };
 
-  imgFilters.addEventListener('click', function (evt) {
-    if (evt.target.type === 'button' && !evt.target.classList.contains('img-filters__button--active')) {
-      changeFilter(evt.target);
-      applyFilter(filterName);
-    }
-  });
+  var imgFilters = document.querySelector('.img-filters');
+  var filterPopular = imgFilters.querySelector('#filter-popular');
+  var filterDiscussed = imgFilters.querySelector('#filter-discussed');
+  var filterNew = imgFilters.querySelector('#filter-new');
+  var imgFiltersButton = document.querySelectorAll('.img-filters__button');
 
   var changeFilter = function (btn) {
     imgFiltersButton.forEach(function (btnItem) {
@@ -52,26 +49,67 @@
     btn.classList.add('img-filters__button--active');
   };
 
-  var applyFilter = function () {
+  // функция для сортировки и показа фотографий по обсуждаемости (в зависимости от количества комментариев)
+  var showDiscussedFotos = function (photos) {
+    var photosCopy = photos.slice();
+    photosCopy.sort(function (first, second) {
+      return second.comments.length - first.comments.length;
+    });
+    insertPhoto(photosCopy);
+  };
 
-    window.arrayPhotos = window.arrayPhotos.slice();
+  // функция для показа 10 новых фотографий (в случайном порядке)
+  var showNewFotos = function (photos) {
+    var photosCopy = photos
+      .sort(function () {
+        return Math.random() - 0.5;
+      })
+      .slice(-NEW_PICTURES);
+    insertPhoto(photosCopy);
+  };
 
-    switch (filterName) {
+  var showLoadSuccess = function (photos) {
+    picturesBlock = photos;
+    imgFilters.classList.remove('img-filters--inactive');
+    insertPhoto(picturesBlock);
+    return picturesBlock;
+  };
 
-      case FilterName.NEW:
-        window.arrayPhotos = window.arrayPhotos
-        .sort(function () {
-          return Math.random() - 0.5;
-        })
-        .slice(-10);
+  var onFilterButtonClick = function (evt) {
+    clearPictures();
+    changeFilter(evt.target);
+    var id = evt.target.id;
+    switch (id) {
+      case 'filter-popular':
+        insertPhoto(picturesBlock);
         break;
-
-      case FilterName.DISCUSSED:
-        window.arrayPhotos = window.arrayPhotos
-          .sort(function (first, second) {
-            return second.comments.length - first.comments.length;
-          });
+      case 'filter-new':
+        showNewFotos(picturesBlock);
         break;
+      case 'filter-discussed':
+        showDiscussedFotos(picturesBlock);
     }
   };
+
+  window.load(showLoadSuccess);
+
+  var debounce = function (fn) {
+    var lastTimeout = null;
+    return function () {
+      var args = arguments;
+      if (lastTimeout) {
+        clearTimeout(lastTimeout);
+      }
+      lastTimeout = setTimeout(function () {
+        fn.apply(null, args);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
+
+  var onFilterButtonClickDebounce = debounce(onFilterButtonClick);
+
+  // меняем подборку фотографий при нажатии на фильтр "Популярные"
+  filterPopular.addEventListener('click', onFilterButtonClickDebounce);
+  filterDiscussed.addEventListener('click', onFilterButtonClickDebounce);
+  filterNew.addEventListener('click', onFilterButtonClickDebounce);
 })();
