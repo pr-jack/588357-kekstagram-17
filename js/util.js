b2f77ea3352cf5f11c4c31847de07e1a46651317
pr-jack <prevgenij@gmail.com>
2019-07-09 15:50:18 +0200

    09.07

--- js/util.js
+++ js/util.js
@@ -15,17 +15,5 @@ window.util = (function () {
         action();
       }
     },
-    isEscEventDisabled: false,
-    errorHandler: function (errorMessage) {
-      var node = document.createElement('div');
-      node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: tomato;';
-      node.style.position = 'absolute';
-      node.style.left = 0;
-      node.style.right = 0;
-      node.style.fontSize = '16px';
-
-      node.textContent = errorMessage;
-      document.body.insertAdjacentElement('afterbegin', node);
-    },
   };
 })();
